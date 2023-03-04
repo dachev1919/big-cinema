8fd4cb2c87b26d863d3fb46e9b943cb9e5fd3f33
Dachev <dachev1919@gmail.com>
2023-03-04 15:00:32 +0200

    fixed bugs

--- src/common/components/modal/Modal.tsx
+++ src/common/components/modal/Modal.tsx
@@ -2,11 +2,7 @@ import React, { FC, useEffect, useState } from 'react';
 import MuiModal from '@mui/material/Modal';
 import { useRecoilState } from 'recoil';
 import { modalState, movieState } from '@/common/atoms/modalAtom';
-import {
-	VolumeOffIcon,
-	VolumeUpIcon,
-	XIcon
-} from '@heroicons/react/outline';
+import { VolumeOffIcon, VolumeUpIcon, XIcon } from '@heroicons/react/outline';
 import { Element, Genre } from '@/@types/typings';
 import ReactPlayer from 'react-player/lazy';
 import styles from './Modal.module.scss';
@@ -22,12 +18,21 @@ const Modal: FC = () => {
 		if (!movie) return;
 
 		async function fetchMovie() {
+			// const data = await fetch(
+			// 	`https://api.themoviedb.org/3/${
+			// 		movie?.media_type === 'tv' ? 'tv' : 'movie'
+			// 	}/${movie?.id}?api_key=${
+			// 		process.env.NEXT_PUBLIC_API_KEY
+			// 	}&language=en-US&append_to_response=videos`
+			// )
+			// 	.then(response => response.json())
+			// 	.catch(error => console.log(error));
 			const data = await fetch(
 				`https://api.themoviedb.org/3/${
 					movie?.media_type === 'tv' ? 'tv' : 'movie'
-				}/${movie?.id}?api_key=${
-					process.env.NEXT_PUBLIC_API_KEY
-				}&language=en-US&append_to_response=videos`
+				}/${
+					movie?.id
+				}?api_key=2df371a6bcba8d998e6152eb1375a2af&language=en-US&append_to_response=videos`
 			)
 				.then(response => response.json())
 				.catch(error => console.log(error));
@@ -76,9 +81,7 @@ const Modal: FC = () => {
 						<button className='modalButton' onClick={() => setMuted(!muted)}>
 							{muted ? <VolumeOffIcon /> : <VolumeUpIcon />}
 						</button>
-						<div className={styles.buttons}>
-						</div>
-
+						<div className={styles.buttons}></div>
 					</div>
 				</div>
 				<div className={styles.info}>
@@ -104,9 +107,7 @@ const Modal: FC = () => {
 									{movie?.original_language}
 								</div>
 								<div className={styles.item}>
-									<span className={styles.span}>
-										Total votes:
-									</span>
+									<span className={styles.span}>Total votes:</span>
 									{movie?.vote_count}
 								</div>
 							</div>
